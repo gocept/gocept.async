22f6264e8ef3ceffe34c6a9092fccddbf8ce005e
Christian Zagrodnick <cz@flyingcircus.io>
2009-02-10 14:57:55 +0000

    first in-progres version of gocept.async

---
+++ src/gocept/async/README.txt
@@ -0,0 +1,51 @@
+Details
++++++++
+
+It is often desirable to process computations asynchronously. Until there was
+`lovely.remotetask` this was not so easy to achieve in a Zope 3 application due
+to transaction integration issues.
+
+`gocept.async` makes the task even easier:
+
+>>> import gocept.async
+>>> @gocept.async.function(service='events')
+... def heavy_computing(a, b):
+...     print "Computing", a, "+", b, "=", a + b
+
+The decorator ``gocept.async.function`` takes exactly one argument, the name of
+a lovely.remotetask.interfaces.ITaskService utility. Note that `gocept.async`
+does **not** define any task service by
+itself[#test-task-service]_[#importable]_.
+
+.. [#importable] Note that the decorated function must have an importable
+    module to be usable:
+
+    >>> import gocept.async.tests
+    >>> heavy_computing.undecorated.__module__ = 'gocept.async.tests'
+    >>> gocept.async.tests.heavy_computing = heavy_computing
+
+.. [#test-task-service] We defined task-service called ``events`` in
+    this test:
+
+    >>> import zope.component
+    >>> import lovely.remotetask
+    >>> import lovely.remotetask.interfaces
+    >>> sm = zope.component.getSiteManager()
+    >>> getRootFolder()['tasks'] = tasks = lovely.remotetask.TaskService()
+    >>> sm.registerUtility(
+    ...     tasks, lovely.remotetask.interfaces.ITaskService, name='events')
+
+When the decorated function is called it returns nothing:
+
+>>> heavy_computing(2, 7)
+
+When we start the processing of the task service, the function is called:
+
+>>> gocept.async.tests.process()
+Computing 2 + 7 = 9
+
+[#cleanup]_
+
+.. [#cleanup] ..
+    >>> sm.registerUtility(
+    ...     tasks, lovely.remotetask.interfaces.ITaskService, name='events')
---
+++ src/gocept/async/task.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2009 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import decorator
+import logging
+import lovely.remotetask.interfaces
+import lovely.remotetask.processor
+import persistent
+import rwproperty
+import zope.app.authentication
+import zope.dottedname
+import zope.security.management
+
+
+log = logging.getLogger(__name__)
+
+
+class TaskDescription(persistent.Persistent):
+
+    def __init__(self, f, args, kwargs):
+        self.f = f
+        self.args = args
+        self.kwargs = kwargs
+        self.principal = self.get_principal()
+
+    @staticmethod
+    def get_principal():
+        interaction = zope.security.management.queryInteraction()
+        if interaction is None:
+            return
+        for p in interaction.participations:
+            return p.principal.id
+
+    @rwproperty.getproperty
+    def f(self):
+        return zope.dottedname.resolve.resolve(self._f)
+
+    @rwproperty.setproperty
+    def f(self, f):
+        if not f.__module__:
+            raise ValueError("Cannot use function without module.")
+        self._f = '%s.%s' % (f.__module__, f.__name__)
+
+
+class AsyncFunction(object):
+
+    zope.interface.implements(lovely.remotetask.interfaces.ITask)
+
+    def __call__(self, service, jobid, input):
+        log.info("Running async function %s" % jobid)
+        self.login(input.principal)
+        input.f(*input.args, **input.kwargs)
+
+
+    @staticmethod
+    def login(principal):
+        if principal is None:
+            return
+        interaction = zope.security.management.getInteraction()
+        participation = interaction.participations[0]
+        auth = zope.component.getUtility(
+            zope.app.security.interfaces.IAuthentication)
+        participation.setPrincipal(auth.getPrincipal(principal))
+
+
+
+def function(service=u''):
+
+    def is_async():
+        interaction = zope.security.management.queryInteraction()
+        if interaction is None:
+            return False
+        try:
+            request = interaction.participations[0]
+        except IndexError:
+            return False
+        return isinstance(
+            request, lovely.remotetask.processor.ProcessorRequest)
+
+    def decorated(f, *args, **kwargs):
+        if is_async():
+            return f(*args, **kwargs)
+        desc = TaskDescription(f, args, kwargs)
+        tasks = zope.component.getUtility(
+            lovely.remotetask.interfaces.ITaskService,
+            name=service)
+        tasks.add(u'gocept.async.function', desc)
+
+    return decorator.decorator(decorated)
+
+
